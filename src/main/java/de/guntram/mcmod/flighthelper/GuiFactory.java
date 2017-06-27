9829f4b4a8f351fea0c0b7c70cbcdb5c80968251
Guntram Blohm <gbl@bso2001.com>
2017-06-27 10:51:56 +0200

    changes for forge 2375

--- src/main/java/de/guntram/mcmod/flighthelper/GuiFactory.java
+++ src/main/java/de/guntram/mcmod/flighthelper/GuiFactory.java
@@ -17,22 +17,12 @@ public class GuiFactory implements IModGuiFactory {
     }
     
     @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass() {
-        return FHGuiConfig.class;
-    }
-    
-    @Override
     public GuiScreen createConfigGui(GuiScreen parentScreen) {
         return new FHGuiConfig(parentScreen);
     }
     
     @Override
     public Set<IModGuiFactory.RuntimeOptionCategoryElement> runtimeGuiCategories() {
-        return null;
-    }
-    
-    @Override
-    public IModGuiFactory.RuntimeOptionGuiHandler getHandlerFor(final IModGuiFactory.RuntimeOptionCategoryElement element) {
         return null;
     }
 }
