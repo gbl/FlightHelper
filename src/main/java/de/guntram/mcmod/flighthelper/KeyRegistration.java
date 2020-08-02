2ffc78e420424e644a6a56d6945380253bd7ddc6
Guntram Blohm <gbl@guntram.de>
2020-08-02 17:12:53 +0200

    update to 1.16.2-pre1

--- src/main/java/de/guntram/mcmod/flighthelper/KeyRegistration.java
+++ src/main/java/de/guntram/mcmod/flighthelper/KeyRegistration.java
@@ -1,11 +1,9 @@
 package de.guntram.mcmod.flighthelper;
 
-import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
-import net.fabricmc.fabric.api.client.keybinding.KeyBindingRegistry;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
@@ -17,18 +15,16 @@ public class KeyRegistration {
     
     public static void registerKeyBindings() {
         final String category="key.categories.flighthelper";
-        KeyBindingRegistry.INSTANCE.addCategory(category);
         
         lockUp    = register("flighthelper:lockup",     GLFW.GLFW_KEY_KP_8, category);
         lockFront = register("flighthelper:lockfront",  GLFW.GLFW_KEY_KP_2, category);
         unlock    = register("flighthelper:unlock",     GLFW.GLFW_KEY_KP_5, category);
-        ClientTickCallback.EVENT.register(e->processKeyBinds());
+        ClientTickEvents.END_CLIENT_TICK.register(e->processKeyBinds());
     }
 
     private static KeyBinding register (String name, int key, String category) {
-        FabricKeyBinding temp;
-        KeyBindingRegistry.INSTANCE.register(
-                temp=FabricKeyBinding.Builder.create(new Identifier(name), InputUtil.Type.KEYSYM, key, category).build());
+        KeyBinding temp=new KeyBinding(name, InputUtil.Type.KEYSYM, key, category);
+        KeyBindingHelper.registerKeyBinding(temp);
         return temp;
     }
     
