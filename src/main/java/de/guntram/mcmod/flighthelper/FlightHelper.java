d456b22ede602efac2a60480f375f3f618822037
Guntram Blohm <gbl@guntram.de>
2021-11-14 16:30:44 +0100

    new config option to snap yaw

--- src/main/java/de/guntram/mcmod/flighthelper/FlightHelper.java
+++ src/main/java/de/guntram/mcmod/flighthelper/FlightHelper.java
@@ -2,11 +2,11 @@ package de.guntram.mcmod.flighthelper;
 
 import de.guntram.mcmod.fabrictools.ConfigurationProvider;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 
-public class FlightHelper implements ClientModInitializer, ClientTickCallback {
+public class FlightHelper implements ClientModInitializer {
     static final String MODID="flighthelper";
     static final String MODNAME="FlightHelper";
     private static float lockedPitch;
@@ -18,27 +18,29 @@ public class FlightHelper implements ClientModInitializer, ClientTickCallback {
         ConfigurationProvider.register(MODNAME, confHandler);
         confHandler.load(ConfigurationProvider.getSuggestedFile(MODID));
         KeyRegistration.registerKeyBindings();
-        ClientTickCallback.EVENT.register(this);
+        ClientTickEvents.END_CLIENT_TICK.register((c) -> tick(c));
     }
 
     public static void lockPitch(float pitch) {
         lockedPitch=pitch;
-        float yaw=MinecraftClient.getInstance().player.getYaw();
-        while (yaw<0) yaw+=360;
-        while (yaw>360) yaw-=360;
-        if (yaw < 5.0 || yaw > 355.0) {
-            yaw=0.0f;
+        if (ConfigurationHandler.getSnapYaw()) {
+            float yaw=MinecraftClient.getInstance().player.getYaw();
+            while (yaw<0) yaw+=360;
+            while (yaw>360) yaw-=360;
+            if (yaw < 5.0 || yaw > 355.0) {
+                yaw=0.0f;
+            }
+            else if (yaw>85.0 && yaw<95.0) {
+                yaw=90.0f;
+            }
+            else if (yaw>175.0 && yaw<185.0) {
+                yaw=180.0f;
+            }
+            else if (yaw>265.0 && yaw<275.0) {
+                yaw=270.0f;
+            }
+            MinecraftClient.getInstance().player.setYaw(yaw);
         }
-        else if (yaw>85.0 && yaw<95.0) {
-            yaw=90.0f;
-        }
-        else if (yaw>175.0 && yaw<185.0) {
-            yaw=180.0f;
-        }
-        else if (yaw>265.0 && yaw<275.0) {
-            yaw=270.0f;
-        }
-        MinecraftClient.getInstance().player.setYaw(yaw);
         isLocked=true;
     }
     
@@ -46,11 +48,10 @@ public class FlightHelper implements ClientModInitializer, ClientTickCallback {
         isLocked=false;
     }
     
-    @Override
-    public void tick(MinecraftClient mncrft) {
+    public void tick(MinecraftClient client) {
         if (!isLocked)
             return;
-        ClientPlayerEntity player = mncrft.player;
+        ClientPlayerEntity player = client.player;
         if (player==null)
             return;                 // logged out while locked
         float curPitch = player.getPitch();
